2c7e6ec61616374ebc7c0993ff32b98f5b2c9bcc
kosickaakademia <roland.onofrej@kosickaakademia.sk>
2021-04-08 10:26:42 +0200

    Added ApiRequest class

---
+++ src/main/java/sk/kosickaakademia/onofrej/exchange/api/ApiRequest.java
@@ -0,0 +1,18 @@
+package sk.kosickaakademia.onofrej.exchange.api;
+
+import java.util.Map;
+import java.util.Set;
+
+public class ApiRequest {
+    public Map getExchangeRates(Set<String> rates){
+        if(rates==null || rates.size() ==0)
+            return null;
+
+
+        return null;
+    }
+
+    private void getRatesFromAPIServer(){
+
+    }
+}

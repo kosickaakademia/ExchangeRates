5b80ad49750c0308802fde17c84a3403fa9dadac
kosickaakademia <roland.onofrej@kosickaakademia.sk>
2021-04-08 09:40:31 +0200

    Initial commit

---
+++ src/main/java/sk/kosickaakademia/onofrej/exchange/Main.java
@@ -0,0 +1,14 @@
+package sk.kosickaakademia.onofrej.exchange;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to the Exchange Rates App!" );
+
+    }
+}

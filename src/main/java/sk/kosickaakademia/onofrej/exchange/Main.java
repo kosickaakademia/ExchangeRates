0516055d090bf45d59b9038429cac4b80f775c93
kosickaakademia <roland.onofrej@kosickaakademia.sk>
2021-04-08 11:50:55 +0200

    Finished ApiRequest class

--- src/main/java/sk/kosickaakademia/onofrej/exchange/Main.java
+++ src/main/java/sk/kosickaakademia/onofrej/exchange/Main.java
@@ -1,5 +1,11 @@
 package sk.kosickaakademia.onofrej.exchange;
 
+import sk.kosickaakademia.onofrej.exchange.api.ApiRequest;
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 /**
  * Hello world!
  *
@@ -9,6 +15,14 @@ public class Main
     public static void main( String[] args )
     {
         System.out.println( "Welcome to the Exchange Rates App!" );
+        Set<String> set = new HashSet<>();
+        set.add("USD");
+        set.add("HUF");
+        set.add("CZK");
+        set.add("AAA");
+        set.add("BTC");
 
+        Map map = new ApiRequest().getExchangeRates(set);
+        System.out.println(map.toString());
     }
 }
--- src/main/java/sk/kosickaakademia/onofrej/exchange/api/ApiRequest.java
+++ src/main/java/sk/kosickaakademia/onofrej/exchange/api/ApiRequest.java
@@ -1,6 +1,14 @@
 package sk.kosickaakademia.onofrej.exchange.api;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.*;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 import java.util.Set;
 
 public class ApiRequest {
@@ -8,11 +16,70 @@ public class ApiRequest {
         if(rates==null || rates.size() ==0)
             return null;
 
+        return parseData(rates);
+    }
 
+    private String getRatesFromAPIServer(){
+        try {
+
+            URL url = new URL("http://api.exchangeratesapi.io/v1/latest?access_key=7f193180007ca666e168d9cd573780e6&format=1");
+
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.connect();
+
+            //Getting the response code
+            int responsecode = conn.getResponseCode();
+
+            if (responsecode != 200) {
+                throw new RuntimeException("HttpResponseCode: " + responsecode);
+            } else {
+
+                String inline = "";
+                Scanner scanner = new Scanner(url.openStream());
+
+                //Write all the JSON data into a string using a scanner
+                while (scanner.hasNext()) {
+                    inline += scanner.nextLine();
+                }
+
+                //Close the scanner
+                scanner.close();
+
+                return inline;
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
-    private void getRatesFromAPIServer(){
+    private Map parseData(Set<String> rates){
+        String inline=getRatesFromAPIServer();
+        if(inline==null)
+            return null;
 
+        try {
+            //Using the JSON simple library parse the string into a json object
+                JSONParser parse = new JSONParser();
+                JSONObject data_obj = (JSONObject) parse.parse(inline);
+
+                //Get the required object from the above created object
+                JSONObject obj = (JSONObject) data_obj.get("rates");
+
+                Map<String,Double> maps = new HashMap<>();
+                for(String temp:rates){
+                    if(obj.containsKey(temp)){
+                        double value= (double)obj.get(temp);
+                        maps.put(temp,value);
+                    }
+                }
+                return maps;
+
+        }catch(Exception ex){
+            ex.printStackTrace();
+        }
+        return null;
     }
 }
